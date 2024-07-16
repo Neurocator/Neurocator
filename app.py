176d0d1efdc75180aa779eb39d594765c9104088
Aanika Tangirala <aanika.tangirala@outlook.com>
2024-07-16 09:20:34 -0700

    main to app

--- app.py
+++ app.py
@@ -10,6 +10,17 @@ def home():
 def forum():
     return render_template('forum.html.j2')
 
+@app.route('/live')
+def live():
+    return render_template("live_assistance.html")
+
+@app.route('/about')
+def about():
+    return render_template("about_us.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
 #if __name__ == '__main__':
 #    app.run(debug=True)
     
