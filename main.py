270ee92d57dd3f153c81a6d30d12e686bb7d0183
Mark Dai <markenever.d@gmail.com>
2024-07-15 19:20:43 -0700

    Co-authored-by: William Yang <wilyan09007@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/forum')
+def forum():
+    return render_template("forum.html")
+
+@app.route('/live')
+def live():
+    return render_template("live_assistance.html")
+
+@app.route('/about')
+def about():
+    return render_template("about_us.html")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
