98ba0c3e301a20e18b6ce44376e895b7f1f8e067
Mark Dai <markenever.d@gmail.com>
2024-07-17 16:56:50 -0700

    app

--- app.py
+++ app.py
@@ -3,26 +3,26 @@ from flask_cors import CORS
 from utils import is_point_covered, process_transcript
 import sqlite3
 
-import mysql.connector
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "Teamb1#123",
-    database = "neurocator",
-    auth_plugin='mysql_native_password'
-)
-cursor = mydb.cursor()
+# import mysql.connector
+# mydb = mysql.connector.connect(
+#     host = "localhost",
+#     user = "root",
+#     password = "Teamb1#123",
+#     database = "neurocator",
+#     auth_plugin='mysql_native_password'
+# )
+# cursor = mydb.cursor()
 
 app = Flask(__name__)
 CORS(app)
 
-# Initialize the SQLite database for the to-do list
-def init_db():
-    with sqlite3.connect('database.db') as conn:
-        c = conn.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS tasks
-                     (task TEXT, completed BOOLEAN)''')
-        conn.commit()
+# # Initialize the SQLite database for the to-do list
+# def init_db():
+#     with sqlite3.connect('database.db') as conn:
+#         c = conn.cursor()
+#         c.execute('''CREATE TABLE IF NOT EXISTS tasks
+#                      (task TEXT, completed BOOLEAN)''')
+#         conn.commit()
 
 @app.route('/')
 def index():
@@ -102,9 +102,7 @@ def complete_task(task_id):
     try:
         with sqlite3.connect('database.db') as conn:
             c = conn.cursor()
-            c.execute('SELECT completed FROM tasks WHERE rowid = ?', (task_id,))
-            completed = c.fetchone()[0]
-            c.execute('UPDATE tasks SET completed = ? WHERE rowid = ?', (not completed, task_id))
+            c.execute('DELETE FROM tasks WHERE rowid = ?', (task_id,))
             conn.commit()
         return redirect(url_for('to_do_list'))
     except Exception as e:
@@ -116,5 +114,5 @@ def faq():
     return render_template('faq.html.j2')
 
 if __name__ == '__main__':
-    init_db()
+    #init_db()
     app.run(host='0.0.0.0', port=8080, debug=True)
