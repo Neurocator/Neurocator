ac89e3b8267f9bab62569634bc68b10437df3fc9
Aanika Tangirala <aanika.tangirala@outlook.com>
2024-07-18 18:32:19 -0700

    forum

--- test.py
+++ test.py
@@ -15,38 +15,58 @@ db_params = {
     'sslmode': 'require'
 }
 
-def insert_data():
-    conn = None
-    cur = None
-    try:
-        logging.info("Connecting to the database...")
-        conn = psycopg2.connect(**db_params)
-        cur = conn.cursor()
+conn = psycopg2.connect(**db_params)
+cur = conn.cursor()
+query = "SELECT id, username, title, content, date FROM posts"
+cur.execute(query)
+conn.commit()
+rows = cur.fetchall()
+posts = []
+for row in rows:
+    post = {
+        'id': row[0], 
+        'username': row[1],
+        'title': row[2],
+        'content': row[3],
+        'date': row[4]
+        }
+    posts.append(post)
+print(posts)
 
-        logging.info("Connected successfully")
+# def insert_data():
+#     conn = None
+#     cur = None
+#     try:
+#         logging.info("Connecting to the database...")
+#         conn = psycopg2.connect(**db_params)
+#         cur = conn.cursor()
 
-        # Example INSERT query
-        query = "INSERT INTO test (name) VALUES (%s)"
-        data = ('test',)
+#         logging.info("Connected successfully")
 
-        logging.info("Executing INSERT query...")
-        cur.execute(query, data)
+#         # Example INSERT query
+#         query = "INSERT INTO test (name) VALUES (%s)"
+#         data = ('test',)
 
-        conn.commit()
-        logging.info("Data inserted successfully")
+#         logging.info("Executing INSERT query...")
+#         cur.execute(query, data)
 
-    except psycopg2.Error as e:
-        logging.error(f"Database error: {e}")
-        if conn:
-            conn.rollback()
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-    finally:
-        if cur:
-            cur.close()
-        if conn:
-            conn.close()
-        logging.info("Database connection closed")
+#         conn.commit()
+#         logging.info("Data inserted successfully")
 
-if __name__ == "__main__":
-    insert_data()
+#     except psycopg2.Error as e:
+#         logging.error(f"Database error: {e}")
+#         if conn:
+#             conn.rollback()
+#     except Exception as e:
+#         logging.error(f"An error occurred: {e}")
+#     finally:
+#         if cur:
+#             cur.close()
+#         if conn:
+#             conn.close()
+#         logging.info("Database connection closed")
+
+# if __name__ == "__main__":
+#     insert_data()
+
+
